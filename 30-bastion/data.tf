c0651a85567418feebdc7fc35a52923d9a5d8b55
MMahiketh <99901318+MMahiketh@users.noreply.github.com>
2024-10-24 18:05:13 +0530

    Created bastion instance

---
+++ 30-bastion/data.tf
@@ -0,0 +1,45 @@
+# Amazon linux 2023 AMI
+data "aws_ami" "az_linux" {
+  owners      = ["137112412989"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+# Join devops practice AMI
+data "aws_ami" "join_devops_ami" {
+  owners      = ["973714476881"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["RHEL*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+# Get security grouop ids
+data "aws_ssm_parameter" "bastion_sg_id" {
+  name = "${local.ssm_prefix}/${var.instances[1]}/sg_id"
+}
+
+# data "aws_ssm_parameter" "ansible_sg_id" {
+#   name = "${local.ssm_prefix}/${var.instances[0]}/sg_id"
+# }
+
+# Get public subnet id
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name = "${local.ssm_prefix}/public/subnet/ids"
+}
---
+++ 30-bastion/main.tf
@@ -0,0 +1,34 @@
+module "bastion" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  ami  = local.ami_id
+  name = "${local.resource_name}-${var.instances[1]}"
+
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [local.bastion_sg_id]
+  subnet_id              = local.public_subnet_id
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${local.resource_name}-${var.instances[1]}" },
+    var.bastion_tags
+  )
+}
+
+# module "ansible" {
+#   source = "terraform-aws-modules/ec2-instance/aws"
+
+#   ami  = local.ami_id
+#   name = "${local.resource_name}-${var.instances[0]}"
+
+#   instance_type          = var.instance_type
+#   vpc_security_group_ids = [local.sg_ids.ansible]
+#   subnet_id              = local.public_subnet_id
+#   user_data              = file("ansible-setup.sh")
+
+#   tags = merge(
+#     local.common_tags,
+#     { Name = "${local.resource_name}-${var.instances[0]}" },
+#     var.ansible_tags
+#   )
+# }
