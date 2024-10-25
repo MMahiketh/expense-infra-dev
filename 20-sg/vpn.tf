83bd2102930c512d8ae25bb7983d8b596b898648
MMahiketh <99901318+MMahiketh@users.noreply.github.com>
2024-10-25 14:21:54 +0530

    Allowed port 945 from internet to vpn

--- 20-sg/vpn.tf
+++ 20-sg/vpn.tf
@@ -81,6 +81,16 @@ resource "aws_security_group_rule" "vpn_internet_943" {
   security_group_id = module.vpn.id
 }
 
+## port 945
+resource "aws_security_group_rule" "vpn_internet_945" {
+  type              = "ingress"
+  from_port         = 945
+  to_port           = 945
+  protocol          = local.protocol
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn.id
+}
+
 ## port 1194
 resource "aws_security_group_rule" "vpn_internet_1194" {
   type              = "ingress"
