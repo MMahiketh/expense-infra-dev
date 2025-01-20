581ac31a4ec1aae30e9016e44a05d9109170e2f3
MMahiketh <99901318+MMahiketh@users.noreply.github.com>
2025-01-20 22:19:43 +0530

    Fixed

--- 90-web-alb/main.tf
+++ 90-web-alb/main.tf
@@ -77,5 +77,5 @@ module "records" {
 resource "aws_ssm_parameter" "alb_listner_arn" {
   name  = "${local.ssm_prefix}/${var.instance}-listner/arn"
   type  = "String"
-  value = aws_alb_listener.http.arn
+  value = aws_lb_listener.https.arn
 }
